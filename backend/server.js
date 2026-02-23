ea67fdc55b56a551f6f24a6d7b71ce5d8eb2e1f0
rizwanwebexpert-sys <rizwanwebexpert@gmail.com>
2026-02-23 16:15:49 +0500

    configure cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 import app from "./app.js";
+import {v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
+});
 
 
 
